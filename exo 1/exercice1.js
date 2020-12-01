58ff50b2a6ba19b9b609ae1b5fd840a848e3b749
BessonMikael <besson.mikael04@gmail.com>
2020-12-01 10:59:06 +0100

    exo 62

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -6,8 +6,8 @@
 
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
-var i;
-( = ;  < ; ) {
+let i = 0;
+for( i = 0; i < 9; i++ ) {
     document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
     }
 
--- exo 2/exercice2.js
+++ exo 2/exercice2.js
@@ -1,9 +1,9 @@
 /* Modifier le code ci dessous pour réaliser l'exercice */
 
 
-for( var i =56 ;  < ; )
+for( let i =56 ;  i>0 ; i-=28 )
 {
-    document.getElementById('monDiv').innerHTML+="Ma variable i vaux "+i+"<br><br>";
+    document.getElementById("monDiv").innerHTML += "Ma variable i vaux " +i+ "<br><br>";
 }
 
 
